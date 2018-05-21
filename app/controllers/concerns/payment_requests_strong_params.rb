f4e00f27ce59710b5e48e354b500f505fafcb44d
Marat Galiev <kazanlug@gmail.com>
2018-05-22 00:33:30 +0300

    Remove unused amount

--- app/controllers/concerns/payment_requests_strong_params.rb
+++
@@ -1,9 +0,0 @@
-module PaymentRequestsStrongParams
-  extend ActiveSupport::Concern
-  protected
-  def payment_request_params
-    params.require(:payment_request).permit(
-      :amount
-    )
-  end
-end
