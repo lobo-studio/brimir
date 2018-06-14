26fa2c0f9614366555236a4f161a1814c75dddf9
Marat Galiev <kazanlug@gmail.com>
2018-06-14 20:26:52 +0300

    Fix 1

--- app/lib/email_processor.rb
+++ app/lib/email_processor.rb
@@ -1,17 +1,157 @@
 class EmailProcessor
+
+  include BounceHelper
+
+  def encode(string)
+    string.encode('UTF-8', invalid: :replace, undef: :replace)
+  end
+
+  def normalize_body(part, charset)
+    # some mail clients apparently send wrong charsets
+    charset = 'utf-8' if charset == 'utf8'
+    encode(part.body.decoded.force_encoding(charset))
+  end
+
   def initialize(email)
     @email = email
   end
 
   def process
-    begin
-      exec("/bin/sh /home/moto/apps/brimir/current/script/post-mail http://tickets.mesbesoinsmoto.com/tickets.json")
-      Rails.logger.info '---------------------------------------------------------------------------'
-      Rails.logger.info @email.inspect
-      Rails.logger.info '---------------------------------------------------------------------------'
-    rescue Exception => e
-      puts 'EXCEPTION'
-      puts @email.inspect
+    email = @email
+    # is this an address verification mail?
+    if VerificationMailer.receive(email)
+      return
+    end
+
+    content = ''
+
+    if email.multipart?
+      if email.html_part
+        content = normalize_body(email.html_part, email.html_part.charset)
+        content_type = 'html'
+      elsif email.text_part
+        content = normalize_body(email.text_part, email.text_part.charset)
+        content_type = 'text'
+      else
+        content = normalize_body(email.parts[0], email.parts[0].charset)
+        content_type = 'html'
+      end
+    else
+      if email.charset
+        content = normalize_body(email, email.charset)
+      else
+        content = encode(email.body.decoded)
+      end
+      if email.content_type.include? 'html'
+        content_type = 'html'
+      else
+        content_type = 'text'
+      end
+    end
+
+    if email.charset
+      subject = encode(email.subject.to_s.force_encoding(email.charset))
+    else
+      subject = email.subject.to_s.encode('UTF-8')
+    end
+
+    if email.in_reply_to
+      # is this a reply to a ticket or to another reply?
+      response_to = Ticket.find_by_message_id(email.in_reply_to)
+
+      if !response_to
+
+        response_to = Reply.find_by_message_id(email.in_reply_to)
+
+        if response_to
+          ticket = response_to.ticket
+        else
+          # we create a new ticket further below in this case
+        end
+      else
+        ticket = response_to
+      end
+    end
+
+    from_address = email.from.first
+    # yes this can get really long...
+    to_addresses = email.to.join ',' unless email.to.nil?
+    cc_addresses = email.cc.join ',' unless email.cc.nil?
+    unless email.reply_to.blank?
+      from_address = email.reply_to.first
+    end
+
+    if response_to
+      # reopen ticket
+      ticket.status = :open
+      ticket.save
+
+      # add reply
+      incoming = Reply.create({
+        content: content,
+        ticket_id: ticket.id,
+        from: from_address,
+        message_id: email.message_id,
+        content_type: content_type,
+        raw_message: StringIO.new(email.to_s),
+        reply_to_id: response_to.try(:id),
+        reply_to_type: response_to.try(:class).try(:name)
+      })
+
+    else
+
+      to_email_address = EmailAddress.find_first_verified_email(
+          email.to.to_a + email.cc.to_a + email.bcc.to_a)
+
+      # add new ticket
+      ticket = Ticket.create({
+        from: from_address,
+        orig_to: to_addresses,
+        orig_cc: cc_addresses,
+        subject: subject,
+        content: content,
+        message_id: email.message_id,
+        content_type: content_type,
+        to_email_address: to_email_address,
+        raw_message: StringIO.new(email.to_s),
+        unread_users: User.where(agent: true),
+        created_at: (email.date || Time.zone.now)
+      })
+
+      incoming = ticket
+
+    end
+
+    if email.has_attachments?
+
+      email.attachments.each do |attachment|
+
+        file = StringIO.new(attachment.decoded)
+
+        # add needed fields for paperclip
+        file.class.class_eval {
+            attr_accessor :original_filename, :content_type
+        }
+
+        file.original_filename = attachment.filename
+        file.content_type = attachment.mime_type
+
+        # store content_id with stripped off '<' and '>'
+        content_id = nil
+        unless attachment.content_id.blank?
+          content_id = attachment.content_id[1..-2]
+          content_id = nil unless incoming.content.include?(content_id)
+        end
+        incoming.attachments.create(file: file,
+            content_id: content_id)
+      end
+
+    end
+
+    if bounced?(email)
+      nil
+    else
+      incoming
     end
   end
 end
